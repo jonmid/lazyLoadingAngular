2c0c0fe36a098b5123064de6af0ae040488b923d
JonMid <10jonmid@gmail.com>
2020-09-29 20:03:02 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { SelectivePreloadingStrategyService } from './services/selective-preloading-strategy.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then((m) => m.CustomersModule),
+    data: { preload: true }, // Utiliza el servicio para Preloading Strategy
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  // imports: [RouterModule.forRoot(routes)], // <-- OPTION 1
+  // imports: [
+  //   // <-- OPTION 2
+  //   RouterModule.forRoot(routes, {
+  //     enableTracing: false, // <-- debugging purposes only
+  //     preloadingStrategy: PreloadAllModules,
+  //   }),
+  // ],
+  imports: [
+    // <-- OPTION 3
+    RouterModule.forRoot(routes, {
+      enableTracing: false, // <-- debugging purposes only
+      preloadingStrategy: SelectivePreloadingStrategyService,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
